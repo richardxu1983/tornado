19973c5650f43ccceb54fa8e3cb9a1437c7e1167
richardxu1983 <2622619554@qq.com>
2016-03-08 23:33:59 +0800

    update

--- static/js/script.js
+++ static/js/script.js
@@ -13,11 +13,28 @@ var Engine = {
             text01 = $('<div>').addClass('text').attr('id', 'text').appendTo('div#area');
             btn = $('<div>').addClass('clickArea').attr('id', 'click').text('click').appendTo('div#area');
 
+            //session
+            document.session = $("#session").val();
+
             btn.click(
                 function(){
-                    text01.text(Math.floor(Math.random() * 100));
+                    text01.text('wait...');
                     btn.css('background-color','#ff9999');
                     btn.animate({backgroundColor:'#6495ed'},100);
+
+                    //ajax
+                    jQuery.ajax({
+                        url:"//localhost:8000",
+                        type:'POST',
+                        data:{
+                            session:document.session,
+                            action:'text'
+                        },
+                        dataType:'json',
+                        success:function(data,status,xhr){
+                            text01.text(data['data']);
+                        }
+                    })
                 }
             );
         }
--- test02.py
+++ test02.py
@@ -1,5 +1,6 @@
 # test02, test for ajax, tornado
 import os.path
+import random
 
 import tornado.ioloop
 import tornado.httpserver
@@ -7,13 +8,24 @@ import tornado.options
 import tornado.web
 import tornado.httpclient
 
+from uuid import uuid4
 from tornado.options import define, options
 define("port", default=8000, help="run on given port", type=int)
 
 # index handler
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html', page_title="hello!")
+        session = uuid4()
+        self.render('index.html', page_title="hello!", session=session)
+
+    def post(self):
+        action = self.get_argument('action')
+        session = self.get_argument('session')
+        if not session:
+            self.set_status(400)
+        if action == 'text':
+            text='back:'+str(random.randint(10, 20))
+            self.write('{"data":"%s"}' % text)
 
 
 class Application(tornado.web.Application):
